7c689880f21162594c01e20357599e45709bc2f6
nikvicros <nikho.rosales@gmail.com>
2020-05-15 17:48:14 -0500

    Testing deps added

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,4 +1,31 @@
+
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const web3 = new Web3(ganache.provider())
+
+class Car {
+
+	park() {
+		return "Stopped"
+	}
+
+	dirve() {
+		return "Vroom"
+	}
+}
+
+const car = new Car();
+
+describe('Car', () => {
+
+	it('Car Park', () => {
+
+		assert.strictEqual(car.park(), 'Stopped');
+	})
+
+	it('Car Drive', () => {
+		assert.strictEqual(car.dirve(), 'Vroom')
+	})
+})
 
