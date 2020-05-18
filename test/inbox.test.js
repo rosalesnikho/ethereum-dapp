301226e4b570ce25d5a743340a64723d707be1a2
nikvicros <nikho.rosales@gmail.com>
2020-05-17 19:06:36 -0500

    Added testing for init msg

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,31 +1,47 @@
-
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider())
 
-class Car {
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
-	park() {
-		return "Stopped"
-	}
+const {interface, bytecode} = require('../compile')
 
-	dirve() {
-		return "Vroom"
-	}
-}
+let accounts;
+let inbox;
 
-const car = new Car();
+beforeEach(async () => {
+	// Get a list of accounts
+	accounts = await web3.eth.getAccounts();
 
-describe('Car', () => {
+	// Use account to deploy a contract
+	inbox = await new web3.eth.Contract(JSON.parse(interface))
+		//Deploys the bytecode contract only
+		.deploy({
+			data: bytecode,
+			arguments: ['inbox-101']
+		})
+		// Sends from the first account created by Ganache and limits gas to 1mil
+		.send({from: accounts[0], gas: '1000000'})
+	inbox.setProvider(provider);
+});
 
-	it('Car Park', () => {
 
-		assert.strictEqual(car.park(), 'Stopped');
+describe('Inbox', () => {
+
+	it('deploys a contract', () => {
+		assert.ok(inbox.options.address)
 	})
 
-	it('Car Drive', () => {
-		assert.strictEqual(car.dirve(), 'Vroom')
+	it('has a default message', async () => {
+		const message = await inbox.methods.message().call();
+		assert.strictEqual(message, 'inbox-101');
 	})
-})
 
+	it('can change the message', async () => {
+		await inbox.methods.setMessage('Genesis').send({from: accounts[0]});
+		const message = await inbox.methods.message().call();
+		assert.strictEqual(message, 'Genesis');
+	})
+
+});
